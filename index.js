64b2a5b8296b396965bfde55e3939786b3387407
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 17:31:30 +0300

    Returns the fare for the customer given the same starting and ending block from distance travelled.

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ function distanceTravelledInFeet(startingBlock, endingBlock) {
 	let distanceTravelled = endingBlock > startingBlock ? (endingBlock - startingBlock)*264 : (startingBlock - endingBlock)*264;
 	return distanceTravelled;
 }
+
+// Returns the fare for the customer given the same starting and ending block from distance travelled
+function calculatesFarePrice(startingBlock, endingBlock) {
+	let distanceInFeet = distanceTravelledInFeet(startingBlock, endingBlock);
+	if(distanceInFeet <= 400) {
+		return 0;
+	}
+	else if(distanceInFeet > 400 && distanceInFeet < 2000) {
+		return (distanceInFeet - 400)*0.02
+	}
+	else if(distanceInFeet > 2000) {
+		if(distanceInFeet > 2500) {
+			return 'cannot travel that far';
+		}
+		return 25;
+	}
+}
