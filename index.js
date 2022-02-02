f3f3ed08620830b49759b0fd0efab6158ab09d26
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 17:27:01 +0300

    Returns distance from Scuber Headquarters to Pickup Location in blocks.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const scuberHeadquarters = 42;
+
+// Returns a distance in blocks and also calculates distances below 42nd street
+function distanceFromHqInBlocks(pickUpLocation) {
+	const distanceInBlocks = pickUpLocation > scuberHeadquarters ? pickUpLocation - scuberHeadquarters : scuberHeadquarters - pickUpLocation;
+	return distanceInBlocks;
+}
